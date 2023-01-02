7536c80f435aa320e9b58ba19309ad5d00d07bfd
gharjare <gharjarechetan@gmail.com>
2023-01-02 13:35:04 +0530

    [chetan gharjare]:program/add unorderd and orderd list obj

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructureAlgorithm
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Datastrucure Algorithm");
+            LinkedList<string> list = new LinkedList<string>();
+            string text =  "A Positive mindset brings positive thinks";
+            string[] textlist = text.Split(" ");
+            foreach(var data in textlist)
+            {
+                list.Add(data);
+                list.Search(data);
+                //list.RemoveFirstNode();
+
+            }
+            list.Display();
+
+            
+        }
+    }
+}
