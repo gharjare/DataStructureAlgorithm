376469b4b79f0642ab631e32811a01f2235a7246
gharjare <gharjarechetan@gmail.com>
2023-01-02 13:39:12 +0530

    [chetan gharjare]Linkedlist/ add program for unorder list

---
+++ LinkedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class LinkedList<T>
+    {
+        public Node head;
+        public void Add(string data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linkedlist", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public Node RemoveFirstNode()
+        {
+            if(this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        public Node Search(string data)
+        {
+            while(this.head != null)
+            {
+                if(this.head.data == data)
+                {
+                    return this.head;
+                }
+                this.head = this.head.next;
+            }
+            return null;
+        }
+       
+        
+
+
+    }
+}
