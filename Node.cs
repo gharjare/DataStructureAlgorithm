7524b99672048b721b82b2131c661ba2ef5fbf00
gharjare <gharjarechetan@gmail.com>
2023-01-02 13:37:30 +0530

    [chetan gharjare]:Node/ add node data for linked list

---
+++ Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructureAlgorithm
+{
+    public class Node
+    {
+        public string data;
+        public Node next;
+
+        public Node(string data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
